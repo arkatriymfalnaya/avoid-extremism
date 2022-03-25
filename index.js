453ec09f821aae05ee46a0a7a0157cc85a3f7d01
Arkady  Rakovsky <arkadyrakovsky@gmail.com>
2022-03-25 09:12:45 +0300

    fix tooltip bug with spaces & add product and company tooltip texts & add setting which posts post to drafts directly

--- index.js
+++ index.js
@@ -3,35 +3,48 @@ const fs = require('fs')
 const path = require('path')
 const yargs = require('yargs')
 
-let replaceWithToolbar = str => str.replace(/[^\[|\/](meta|instagram|facebook|инстаграм|мета|фейсбук)[а-яА-Я]*/gi, a => {
-  let start = a[0]
-  let updatedStr = a.substring(1)
+let replaceWithToolbar = str => str.replace(/[^\[|\/](meta|instagram|facebook|инстаграм|мета|фейсбук)[а-яА-Я]*/gi, (subStr, _, subStrIndex) => {
+  let nextSymbol = str[subStrIndex + subStr.length]
+  
+  if(str[subStrIndex - 3] + str[subStrIndex - 2] + str[subStrIndex - 1] + str[subStrIndex] === 'www.') return subStr
 
-  return `${start}[su_tooltip text="Организация признана экстремистской на территории Российской Федерации."]<code> </code>${updatedStr}*<code> </code>[/su_tooltip]`
+  let space = '<code style="letter-spacing: -7px;"> </code>'
+
+  let start = subStr[0]
+  let end = nextSymbol === ' ' ? space : ''
+  let updatedStr = subStr.substring(1)
+
+  let tooltipText =
+    subStr.includes('нстагра') || subStr.includes('ейсб') || subStr.includes('nstagr') || subStr.includes('aceboo')
+      ? 'Продукт принадлежит организации, признанной экстремистской на территории Российской Федерации.'
+      : 'Организация признана экстремистской на территории Российской Федерации.'
+
+  return `${start}[su_tooltip text="${tooltipText}"]${space}${updatedStr}*[/su_tooltip]${end}`
 })
 
 let entry = async (fileName) => {
   const data = fs.readFileSync(path.join(__dirname, fileName), 'utf8')
-  
   const parser = new XMLParser()
   let jObj = parser.parse(data)
-
+  
   let metasArray = jObj.rss.channel.item['wp:postmeta']
   let newMetasArray = metasArray.map(m => {
     if(m['wp:meta_key'] === '_crb_description' || m['wp:meta_key'] === '_crb_short_description') {
       let newMeta = replaceWithToolbar(m['wp:meta_value'])
       m['wp:meta_value'] = newMeta
     }
-
+    
     return m
   })
   jObj.rss.channel.item['wp:postmeta'] = newMetasArray
-
+  
   let content = jObj.rss.channel.item['content:encoded']
   let newContent = replaceWithToolbar(content)
   jObj.rss.channel.item['content:encoded'] = newContent
-
-  const builder = new XMLBuilder({ processEntities:false })
+  
+  jObj.rss.channel.item['wp:status'] = '<![CDATA[draft]]>'
+  
+  const builder = new XMLBuilder({ processEntities: false })
   const xmlContent = builder.build(jObj)
   fs.writeFileSync(path.join(__dirname, `output_${fileName}`), xmlContent)
 }
