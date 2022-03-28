eac9a36b8f2a5a6a9723f42b523d78dc04930809
Arkady  Rakovsky <arkadyrakovsky@gmail.com>
2022-03-28 13:21:55 +0300

    fix html tags parsing

--- index.js
+++ index.js
@@ -1,57 +1,101 @@
 const { XMLParser, XMLBuilder } = require('fast-xml-parser')
+const posthtml = require('posthtml')
 const fs = require('fs')
 const path = require('path')
 const yargs = require('yargs')
 
-let replaceWithFootnote = (str, isTitle = false) => str.replace(/[^\[|\/](meta|instagram|facebook|инстаграм|мета|фейсбук)[а-яА-Я]*/gi, (subStr, _, subStrIndex) => {
-  let nextSymbol = str[subStrIndex + subStr.length]
-  
-  if(str[subStrIndex - 3] + str[subStrIndex - 2] + str[subStrIndex - 1] + str[subStrIndex] === 'www.') return subStr
+const exRegExp = /(meta|instagram|facebook|инстаграм|мета|фейсбук)[а-яА-Я]*/gui
+
+let gotMatchInsideMd = str => {
+  let m = str.match(/\[\/[a-zA-Z]\w*/)
+  if(m) {
+    let tagName = m[0].replace('[/', '')
+    let tagContent = str.match(new RegExp('(.*?)' + `\\/${tagName}`, 'ui')) || str.match(new RegExp(`\\[${tagName}` + '.*', 'ui'))
+
+    if(exRegExp.test(tagContent[1])) return true
+  }
+
+  return false
+}
+
+let replaceWithFootnote = (str, isTitle = false) => str.replace(exRegExp, (subStr, _, subStrIndex) => {
+  let nextIndex = subStrIndex + subStr.length
+  let nextSymbol = str[nextIndex]
+
+  if(
+    subStr.includes('вселен') ||
+    subStr.includes('vers')
+  ) return subStr
 
   let space = '<code style="letter-spacing: -7px;"> </code>'
 
-  let start = subStr[0]
+  let start = str[subStrIndex - 1] === ' ' ? space : ''
   let end = nextSymbol === ' ' ? space : ''
-  let updatedStr = subStr.substring(1)
 
   let tooltipText =
     subStr.includes('нстагра') || subStr.includes('ейсб') || subStr.includes('nstagr') || subStr.includes('aceboo')
       ? 'Продукт принадлежит организации, признанной экстремистской на территории Российской Федерации.'
       : 'Организация признана экстремистской на территории Российской Федерации.'
 
-  let res = isTitle ? `${subStr}*` : `${start}${space}[su_tooltip text="${tooltipText}" text_align="center"]${updatedStr}[/su_tooltip]${end}`
+  let res = isTitle ? `${subStr}*` : `${start}[su_tooltip text="${tooltipText}" text_align="center"]${subStr}[/su_tooltip]${end}`
   return res
 })
+
+let replaceTreeContentRecursively = node => {
+  if(node.content) {
+    if(node.content.length) {
+      if(node.content.length === 1 && typeof node.content[0] === 'string') {
+        node.content[0] = replaceWithFootnote(node.content[0], gotMatchInsideMd(node.content[0]))
+      } else {
+        for(let i = 0; i <= node.content.length - 1; i++) {
+          node.content[i] = replaceTreeContentRecursively(node.content[i])
+        }
+      }
+    }
+  } else if (typeof node === 'string') {
+    node = replaceWithFootnote(node, gotMatchInsideMd(node)) 
+  }
+
+  return node
+}
 
 let entry = async (fileName) => {
   const data = fs.readFileSync(path.join(__dirname, fileName), 'utf8')
   const parser = new XMLParser()
   let jObj = parser.parse(data)
 
-  let title = jObj.rss.channel.item['title']
-  let newTitle = replaceWithFootnote(title, true)
-  jObj.rss.channel.item['title'] = newTitle
+  let items = !Array.isArray(jObj.rss.channel.item) ? [ jObj.rss.channel.item ] : jObj.rss.channel.item
 
-  let metasArray = jObj.rss.channel.item['wp:postmeta']
-  let newMetasArray = metasArray.map(m => {
-    if(m['wp:meta_key'] === '_crb_description' || m['wp:meta_key'] === '_crb_short_description') {
-      let newMeta = replaceWithFootnote(m['wp:meta_value'])
-      m['wp:meta_value'] = newMeta
-    }
-    
-    return m
-  })
-  jObj.rss.channel.item['wp:postmeta'] = newMetasArray
+  for(let i = 0; i <= items.length - 1; i++) {
+    let title = items[i]['title']
+    let newTitle = replaceWithFootnote(title, true)
+    items[i]['title'] = newTitle
+  
+    let metasArray = items[i]['wp:postmeta']
+    let newMetasArray = metasArray.map(m => {
+      if(m['wp:meta_key'] === '_crb_description' || m['wp:meta_key'] === '_crb_short_description') {
+        let newMeta = replaceWithFootnote(m['wp:meta_value'])
+        m['wp:meta_value'] = newMeta
+      }
+      
+      return m
+    })
+    items[i]['wp:postmeta'] = newMetasArray
+  
+    let content = items[i]['content:encoded']
+    let newContent = posthtml().use(tree => {
+      for(let i = 0; i <= tree.length - 1; i++) {
+        tree[i] = replaceTreeContentRecursively(tree[i])
+      }
 
-  let content = jObj.rss.channel.item['content:encoded']
-  let newContent = replaceWithFootnote(content)
-  jObj.rss.channel.item['content:encoded'] = newContent
-  
-  jObj.rss.channel.item['wp:status'] = '<![CDATA[draft]]>'
-  
-  const builder = new XMLBuilder({ processEntities: false })
-  const xmlContent = builder.build(jObj)
-  fs.writeFileSync(path.join(__dirname, `output_${fileName}`), xmlContent)
+      return tree
+    }).process(content, { sync: true }).html
+    items[i]['content:encoded'] = newContent
+  }
+
+  const itemsBuilder = new XMLBuilder({ arrayNodeName: 'item' })
+  const xmlItems = itemsBuilder.build(items)
+  fs.writeFileSync(path.join(__dirname, `output_${fileName}`), xmlItems)
 }
 
 const options = yargs
